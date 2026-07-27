5cbd8b797f18118204fd516ef1c245900cab9f16
oeduarda19-creator <o.eduarda19@escola.pr.gov.br>
2026-07-27 15:15:28 +0000

    v

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const botoes = document.querySelectorAll("button");
-botoes.forEach(function(botao) {
+botoes.forEach(function(botao) 
 botao.addEventListener("click", botaoClicado);
 
 function botaoClicado() {
 console.log("fui clicado");
 let texto = botao.querySelector("span");
-texto.textContent++;
+if (curtir === false)
+    texto.textContent++;
+curtir=true
 }
-});
